b4b55e28b8f97a270994fc475e65728ab7736c24
eugen <Eugen1017>
2025-04-10 13:29:44 +0300

    update header file for tasl logik

---
+++ include/task.h
@@ -0,0 +1,30 @@
+#ifndef TASK_H
+#define TASK_H
+
+#include <string>
+#include <ctime> //for fate and time
+
+class Task {
+private:
+	int id;
+	std::string title;
+	bool isCompleted;
+	std::time_t dueDate;
+
+public:
+	Task(int id, const std::string &title, bool isCompleted=false, std::time_t dueDate=0);
+
+	int getId() const;
+	std::string getTitle() const;
+	bool getIsCompleted() const;
+	std::time_t getDueDate() const;
+
+	void setTitle(const std::string &newTitle);
+	void marksAsCompleted();
+	void setDueDate(std::time_t newDueDate);
+
+	std::string toSQLInsert() const;
+	static Task fromSQLRow(char **row);
+};
+
+#endif
